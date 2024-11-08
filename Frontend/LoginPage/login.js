5c154b92f0e2593616bdb0120565167ac64f7e0b
catyuzu23 <cfruja@gmail.com>
2024-11-08 19:55:48 +0200

    Message after login is pressed
    -to fetch with the api

---
+++ Frontend/LoginPage/login.js
@@ -0,0 +1,6 @@
+const loginButton = document.querySelector('.login-button');
+
+// Show message on mouseover
+loginButton.addEventListener('click', () => {
+  clickLogin.style.display = 'block'; // Show the message
+});
